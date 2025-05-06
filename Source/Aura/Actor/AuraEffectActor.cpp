0e9cec11b74ed12a52188d1f393de344f4b5ec34
jiyunkim201611126 <103517141+jiyunkim201611126@users.noreply.github.com>
2025-05-06 20:03:08 +0900

    섹션 6-45 완료
    AuraEffectActor 리팩토링, 확장성 확보
    여러 이펙트를 가짐은 물론 트리거 타이밍도 다르게 지정 가능
    Infinite Effect 적용
    Effect Stacking에 대한 이해 및 FireArea에 Stacking 적용

--- Source/Aura/Actor/AuraEffectActor.cpp
+++ Source/Aura/Actor/AuraEffectActor.cpp
@@ -15,23 +15,59 @@ void AAuraEffectActor::BeginPlay()
 	Super::BeginPlay();
 }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
 {
-	// FunctionLibrary를 통해 Target의 AbilitySystemComponent 가져오기
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (TargetASC == nullptr) return;
+
+	// GameplayEffects를 순회
+	for (auto& Effect : GameplayEffects)
+	{
+		// Overlap할 때 Effect를 적용해야 하는 경우
+		if (Effect.ApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+		{
+			check(Effect.GameplayEffectClass);
+
+			// GameplayEffect가 어떻게 적용됐는지에 대한 정보를 가진 구조체 선언
+			// 누가 때렸는지, 누가 맞았는지, 어떤 속성의 공격인지, 데미지는 몇인지 등 로그 같은 개념
+			FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	
+			// 어떤 객체에 의해 발생한 Effect인지 추가
+			EffectContextHandle.AddSourceObject(this);
+	
+			// GameplayEffectSpecHandle 생성, Spec이란 Effect의 틀로서, 여러 곳에서 같은 효과를 적용하고 싶을 때 사용하면 좋음
+			FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(Effect.GameplayEffectClass, 1.f, EffectContextHandle);
+
+			// Spec 없이 재사용을 염두하지 않고 간편하게 사용하는 경우 ApplyGameplayEffectToSelf 호출
+			// 또한 원하는 타이밍에 제거하고 싶은 경우 추적할 수 있도록 구조체 내에 할당
+			Effect.ActiveGameplayEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+		}
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
 	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if (TargetASC == nullptr) return;
 	
-	check(GameplayEffectClass);
-	
-	// GameplayEffect가 어떻게 적용됐는지에 대한 정보를 가진 구조체 선언
-	// 누가 때렸는지, 누가 맞았는지, 어떤 속성의 공격인지, 데미지는 몇인지 등 로그 같은 개념
-	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
-	
-	// 어떤 객체에 의해 발생한 Effect인지 추가
-	EffectContextHandle.AddSourceObject(this);
-	
-	// GameplayEffectSpecHandle 생성, Spec이란 Effect의 틀로서, 여러 곳에서 같은 효과를 적용하고 싶을 때 사용하면 좋음
-	// Spec 없이 재사용을 염두하지 않고 간편하게 사용하는 경우 ApplyGameplayEffectToSelf 호출
-	FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	for (auto& Effect : GameplayEffects)
+	{
+		// EndOverlap할 때 Effect를 적용해야 하는 경우
+		if (Effect.ApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		{
+			check(Effect.GameplayEffectClass);
+			FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+			EffectContextHandle.AddSourceObject(this);
+			FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(Effect.GameplayEffectClass, 1.f, EffectContextHandle);
+			Effect.ActiveGameplayEffectHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+		}
+		
+		if (Effect.RemovalPolicy == EEffectRemovalPolicy::RemoveOnEndOverlap)
+		{
+			check(Effect.GameplayEffectClass);
+
+			// OnOverlap에서 추적할 수 있도록 할당해뒀으므로 제거 가능
+			TargetASC->RemoveActiveGameplayEffect(Effect.ActiveGameplayEffectHandle, 1);
+		}
+	}
 }
--- Source/Aura/Actor/AuraEffectActor.h
+++ Source/Aura/Actor/AuraEffectActor.h
@@ -2,9 +2,49 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
 #include "AuraEffectActor.generated.h"
 
 class UGameplayEffect;
+class UAbilitySystemComponent;
+
+// 트리거 작동 시 Effect 적용을 어떻게 처리할 것인지에 대한 enum 
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+// Infinite 적용 상태인 경우 언제 Effect를 제거할 것인지에 대한 enum
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy
+{
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
+
+USTRUCT(BlueprintType)
+struct FGameplayEffectInfo
+{
+	GENERATED_BODY()
+
+	// 적용할 Effect
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TSubclassOf<UGameplayEffect> GameplayEffectClass;
+
+	// 적용 시점
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	EEffectApplicationPolicy ApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	// 제거 시점
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	EEffectRemovalPolicy RemovalPolicy = EEffectRemovalPolicy::RemoveOnEndOverlap;
+
+	// 제거가 필요한 경우 추적을 위해 사용
+	FActiveGameplayEffectHandle ActiveGameplayEffectHandle;
+};
 
 /**
  * GameplayEffect만 처리하는 액터 클래스
@@ -24,11 +64,15 @@ protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void OnOverlap(AActor* TargetActor);
 	
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
-	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
-	
+	bool bDestroyOnEffectRemoval = false;
+
+	// 해당 클래스를 상속받는 자손 블루프린트 액터의 디테일 패널에서 Element를 추가
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
-	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+	TArray<FGameplayEffectInfo> GameplayEffects;
 };
