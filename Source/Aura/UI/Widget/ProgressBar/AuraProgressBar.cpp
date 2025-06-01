0d37fa986a48b2489e4c3beb07f5c8e1b814117a
jiyunkim201611126 <103517141+jiyunkim201611126@users.noreply.github.com>
2025-06-01 15:57:20 +0900

    ProgressBar 상속 구조 설계
    ProgressBar 로직을 AuraProgressBar에 구현
    적 체력바와 캐릭터의 체력구, 마나구가 AuraProgessBar를 상속받아 구현됨

---
+++ Source/Aura/UI/Widget/ProgressBar/AuraProgressBar.cpp
@@ -0,0 +1,49 @@
+﻿#include "AuraProgressBar.h"
+
+#include "Kismet/KismetMathLibrary.h"
+#include "Components/ProgressBar.h"
+
+void UAuraProgressBar::SetBarPercent_Implementation(const float Value, const float MaxValue)
+{
+	const float Percent = UKismetMathLibrary::SafeDivide(Value, MaxValue);
+	ProgressBar_Front->SetPercent(Percent);
+
+	FTimerHandle PercentSetTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(
+		PercentSetTimerHandle,
+		this,
+		&UAuraProgressBar::BarPercentSet,
+		GhostStartDelay,
+		false);
+}
+
+void UAuraProgressBar::BarPercentSet_Implementation()
+{
+	GhostPercentTarget = ProgressBar_Front->GetPercent();
+
+	GetWorld()->GetTimerManager().ClearTimer(GhostPercentSetTimerHandle);
+	GetWorld()->GetTimerManager().SetTimer(
+		GhostPercentSetTimerHandle,
+		this,
+		&UAuraProgressBar::InterpGhostBar,
+		GhostInterpDelay,
+		false);
+}
+
+void UAuraProgressBar::InterpGhostBar_Implementation()
+{
+	const float CurrentGhostBarPercent = ProgressBar_Ghost->GetPercent();
+	const float NextGhostBarPercent = UKismetMathLibrary::FInterpTo(CurrentGhostBarPercent, GhostPercentTarget, GetWorld()->GetDeltaSeconds(), GhostInterpSpeed);
+	ProgressBar_Ghost->SetPercent(NextGhostBarPercent);
+
+	if (!UKismetMathLibrary::NearlyEqual_FloatFloat(NextGhostBarPercent, GhostPercentTarget))
+	{
+		GetWorld()->GetTimerManager().ClearTimer(GhostPercentSetTimerHandle);
+		GetWorld()->GetTimerManager().SetTimer(
+			GhostPercentSetTimerHandle,
+			this,
+			&UAuraProgressBar::InterpGhostBar,
+			GhostInterpDelay,
+			false);
+	}
+}
---
+++ Source/Aura/UI/Widget/ProgressBar/AuraProgressBar.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Aura/UI/Widget/AuraUserWidget.h"
+#include "AuraProgressBar.generated.h"
+
+class UProgressBar;
+
+/**
+ * ProgressBar 2개로 구현되는 프로젝트 기본 ProgressBar입니다.
+ * Front는 값이 바로 변경되며, Ghost는 타이머를 통해 Front를 뒤따라갑니다.
+ * 3개의 함수를 통해 Ghost가 조정되며, 블루프린트에서 오버라이드해 사용하는 것도 가능합니다.
+ * 단, Size 조정이나 FillImage 등은 블루프린트에서 PreConstruct를 통해 초기화합니다.
+ */
+
+UCLASS()
+class AURA_API UAuraProgressBar : public UAuraUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void SetBarPercent(const float Value, const float MaxValue);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void BarPercentSet();
+
+	UFUNCTION(BlueprintNativeEvent)
+	void InterpGhostBar();
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true", BindWidget = "true"))
+	TObjectPtr<UProgressBar> ProgressBar_Front;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true", BindWidget = "true"))
+	TObjectPtr<UProgressBar> ProgressBar_Ghost;
+
+	FTimerHandle GhostPercentSetTimerHandle;
+
+	float GhostPercentTarget = 0.5f;
+	float GhostStartDelay = 1.f;
+	float GhostInterpDelay = 0.02f;
+	float GhostInterpSpeed = 3.f;
+};
