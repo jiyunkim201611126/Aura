3f053659ad5b9e2804de23c90cd8b00d6ca3db40
jiyunkim201611126 <103517141+jiyunkim201611126@users.noreply.github.com>
2025-06-16 00:22:31 +0900

    섹션 15-171 완료
    Decorator 및 Task를 이용해 근거리 공격 AI 캐릭터의 플레이어 추적 로직 작성

--- Source/Aura/Character/AuraEnemy.cpp
+++ Source/Aura/Character/AuraEnemy.cpp
@@ -49,11 +49,10 @@ void AAuraEnemy::PossessedBy(AController* NewController)
 		return;
 	}
 	AuraAIController = Cast<AAuraAIController>(NewController);
-	if (AuraAIController)
-	{
-		AuraAIController->GetBlackboardComponent()->InitializeBlackboard(*BehaviorTree->BlackboardAsset);
-		AuraAIController->RunBehaviorTree(BehaviorTree);
-	}
+	AuraAIController->GetBlackboardComponent()->InitializeBlackboard(*BehaviorTree->BlackboardAsset);
+	AuraAIController->RunBehaviorTree(BehaviorTree);
+	AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("HitReacting"), false);
+	AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("RangedAttacker"), CharacterClass != ECharacterClass::Warrior);
 }
 
 void AAuraEnemy::HighlightActor()
@@ -134,6 +133,7 @@ void AAuraEnemy::HitReactTagChanged(const FGameplayTag CallbackTag, int32 NewCou
 {
 	bHitReacting = NewCount > 0;
 	GetCharacterMovement()->MaxWalkSpeed = bHitReacting ? 0.f : BaseWalkSpeed;
+	AuraAIController->GetBlackboardComponent()->SetValueAsBool(FName("HitReacting"),bHitReacting);
 }
 
 void AAuraEnemy::InitAbilityActorInfo()
