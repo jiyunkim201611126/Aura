945b1cfd2a0006267f7741152d01aa8a18578079
jiyunkim201611126 <103517141+jiyunkim201611126@users.noreply.github.com>
2025-11-01 19:24:42 +0900

    섹션 30-399 완료
    체크포인트 구현 중

---
+++ Source/Aura/Checkpoint/Checkpoint.cpp
@@ -0,0 +1,49 @@
+﻿#include "Checkpoint.h"
+
+#include "Components/SphereComponent.h"
+
+ACheckpoint::ACheckpoint(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(CheckpointMesh);
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void ACheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereOverlap);
+}
+
+void ACheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (APawn* OtherPawn = Cast<APawn>(OtherActor))
+	{
+		if (OtherPawn->IsLocallyControlled())
+		{
+			ActiveGlowEffects();
+	
+			// 이미 한 번 작동한 Checkpoint이므로, SphereComponent가 더이상 이벤트를 발생시킬 수 없도록 설정합니다.
+			Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+			Sphere->OnComponentBeginOverlap.Clear();
+		}
+	}
+}
+
+void ACheckpoint::ActiveGlowEffects()
+{
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	CheckpointMesh->SetMaterial(0, DynamicMaterialInstance);
+	CheckpointReached(DynamicMaterialInstance);
+}
---
+++ Source/Aura/Checkpoint/Checkpoint.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "Checkpoint.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class AURA_API ACheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+
+public:
+	ACheckpoint(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	//~ Begin AActor Interface
+	virtual void BeginPlay() override;
+	//~ End of AActor Interface
+	
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	void ActiveGlowEffects();
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
